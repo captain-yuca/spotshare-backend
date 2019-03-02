d4146bf9f3ca689c05734f712fb750b223e50fc9
captain-yuca <manuel.baez@upr.edu>
2019-03-02 14:44:07 -0500

    Added config for heroku.

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,6 @@
 // Update with your config settings.
+var pg = require('pg')
+pg.defaults.ssl = true
 
 module.exports = {
 
@@ -18,12 +20,9 @@ module.exports = {
   },
 
   staging: {
-    client: 'mysql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
+    client: 'pg',
+    ssl: true,
+    connection: process.env.DATABASEURL || 'postgres://rbjnkmrmvswbsd:234c9f3f87f6ab4e6efe0684dd271ebdb5d139d6ef29727db763868680d12314@ec2-107-21-224-76.compute-1.amazonaws.com:5432/dfvsuusq2gl78d',
     pool: {
       min: 2,
       max: 10
