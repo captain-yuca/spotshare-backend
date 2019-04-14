35449335628b1940331e454b12b05f5461b5c507
captain-yuca <manuel.baez@upr.edu>
2019-04-14 14:10:25 -0400

    Made sure jest isn't hanging in tests.

--- test/teardown.js
+++ test/teardown.js
@@ -1,10 +1,12 @@
 /* eslint-env jest */
 
 module.exports = async function (done) {
+  console.log('Starting Teardown')
   const knexConfig = require('../knexfile')
   const knexCleaner = require('knex-cleaner')
   const Knex = require('knex')
   const knex = Knex(knexConfig.testing)
   await knexCleaner.clean(knex).then({})
   await knex.destroy()
+  console.log('Finished Teardown')
 }
